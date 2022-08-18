1d5d4a4c64989af65344748bf643e77add12588a
Stephen Justin <stevejustin1963@gmail.com>
2022-08-18 23:03:09 +1000

    Update rtc.asm

--- rtc.asm
+++ rtc.asm
@@ -21,9 +21,9 @@ SDA_bit	equ	0h		; SDA port PC.0
 ; When finished, the procedure will jump straight to the CA80 Display Time program *E[0]
 
 ;The code is synchronizing the CA80 time with the RTC. It is first initiating the bus, 
-then setting the address counter to 00h, starting the i2c, putting the byte 0D1h, getting the ACK, 
-getting the byte, putting the byte in (hl), 
-sending the ACK, getting the byte, putting the byte in (hl), and then increments hl.
+;then setting the address counter to 00h, starting the i2c, putting the byte 0D1h, getting the ACK, 
+;getting the byte, putting the byte in (hl), 
+;sending the ACK, getting the byte, putting the byte in (hl), and then increments hl.
 
 
 GET_RTC:
@@ -57,12 +57,12 @@ GETTIME: ; Synch CA80 time with RTC
     
     
     ; This code is responsible for getting the current date and time from a server and storing it in memory. 
-    The "call send_ack" command sends a signal to the server indicating that the client is ready to receive data. 
-    The "call getbyte" command gets a single byte of data from the server, which is then stored 
-    in memory at the location pointed to by HL. The "ld (hl),
-    a" command loads the byte into the location specified by HL. 
-    The "inc hl" command increments the HL register, which points to the next byte in memory. 
-    This process is repeated until all three bytes (hours, date, and month) have been received and stored in memory.
+   ; The "call send_ack" command sends a signal to the server indicating that the client is ready to receive data. 
+   ; The "call getbyte" command gets a single byte of data from the server, which is then stored 
+   ; in memory at the location pointed to by HL. The "ld (hl),
+   ; a" command loads the byte into the location specified by HL. 
+   ; The "inc hl" command increments the HL register, which points to the next byte in memory. 
+   ; This process is repeated until all three bytes (hours, date, and month) have been received and stored in memory.
     
     call send_ack
 	call getbyte		; get hours
@@ -82,12 +82,12 @@ GETTIME: ; Synch CA80 time with RTC
 	
 	
 ; The code gets a byte (the year) and loads it into the memory location pointed to by HL. 
-It then increments HL (so it points to the next byte) and calls send_noack. 
-This sends the byte without sending an acknowledgement. 
-The code then calls stop, which halts the processor.
-RST 10h is a instruction to clear the display. 
-defb 80h is a directive to define a byte with the value 80h. 
-JP M0 is an instruction to jump to the label M0.	
+;;It then increments HL (so it points to the next byte) and calls send_noack. 
+;This sends the byte without sending an acknowledgement. 
+;;The code then calls stop, which halts the processor.
+;RST 10h is a instruction to clear the display. 
+;defb 80h is a directive to define a byte with the value 80h. 
+;JP M0 is an instruction to jump to the label M0.	
 	
 	
     call getbyte            ; get year
@@ -106,9 +106,9 @@ JP M0 is an instruction to jump to the label M0.
 ;
 ; 
 ; This code saves the software RTC to the HW RTC. It starts by setting port C(hi) and C(low) to output and port B to output. 
-It then saves the seconds to EEPROM under address 00. 
-It does the same for the minutes, hours, day, month, and year. 
-Finally, it stops and resets.
+;It then saves the seconds to EEPROM under address 00. 
+;It does the same for the minutes, hours, day, month, and year. 
+;Finally, it stops and resets.
 
 
 	ORG 2100H  ; So I can remember the address for SAVETIME procedure
@@ -181,12 +181,12 @@ SAVETIME:	; save current software RTC to HW RTC procedure, call by: *E[G][2100]=
 
 
 ; 
-This code delays for a certain amount of time. 
-First, it pushes the AF, BC, and DE registers onto the stack. 
-Then, it loads the DE register with the value 0400h. 
-Next, it decrements the DE register and checks if it is equal to zero. 
-If it is not, the code jumps back to the W40 label and repeats the process. 
-Once the DE register is equal to zero, the code pops the AF, BC, and DE registers off the stack and returns.
+;This code delays for a certain amount of time. 
+;First, it pushes the AF, BC, and DE registers onto the stack. 
+;Then, it loads the DE register with the value 0400h. 
+;Next, it decrements the DE register and checks if it is equal to zero. 
+;If it is not, the code jumps back to the W40 label and repeats the process. 
+;Once the DE register is equal to zero, the code pops the AF, BC, and DE registers off the stack and returns.
 
 WAIT_4:	; delay
 		push	AF
@@ -210,7 +210,7 @@ W40:	djnz W40
 
 
 ; The code is sending a write command to an i2c device on address D0, and then reading from address 00h. 
-The purpose of the code is to set the address of the i2c device to 00h so that it can be read from.
+;The purpose of the code is to set the address of the i2c device to 00h so that it can be read from.
 
 set_addr:
 					; Reset device address counter to 00h, for i2c device on address D0
@@ -241,9 +241,9 @@ set_addr:
 
 
 ; This code is checking for an acknowledgement from an I2C slave. SDA is set to input and SCL is set to output. 
-A clock pulse is generated and then SDA is read. 
-If SDA is low, then an acknowledgement was received. 
-If SDA is high, then either no acknowledgement was received or there was a timeout.
+;A clock pulse is generated and then SDA is read. 
+;If SDA is low, then an acknowledgement was received. 
+;If SDA is high, then either no acknowledgement was received or there was a timeout.
 
 get_ack:	; Get ACK from i2c slave
     ld A,093h			; SDA - in, SCL - out 
@@ -268,9 +268,9 @@ send_ack: ld a,092h	; SDA + SCL output
 
 
 ; The code is sending a NAK signal to the I2C bus. 
-A NAK is a "no acknowledge" signal, which is used to indicate to the master that the slave is not ready or does not understand the command. 
+;A NAK is a "no acknowledge" signal, which is used to indicate to the master that the slave is not ready or does not understand the command. 
 ; The code is sending a NAK signal to the I2C bus. 
-A NAK is a "no acknowledge" signal, which is used to indicate to the master that the slave is not ready or does not understand the command.
+;A NAK is a "no acknowledge" signal, which is used to indicate to the master that the slave is not ready or does not understand the command.
 
 send_noack:		; Send NAK (no ACK) to i2c bus (master keeps SDA HI on the 9th bit of data)
 	ld a,092h     		
@@ -282,12 +282,12 @@ send_noack:		; Send NAK (no ACK) to i2c bus (master keeps SDA HI on the 9th bit 
 	
 
 ; This code reads 8 bits from an i2c bus. 
-It starts by pushing the BC register onto the stack. 
-It then sets the SDA line to be an input and the SCL line to be an output. 
-It then calls the sclset function to set the clock line high. 
-It reads the SDA line to get the data bit and then stores it in the C register. 
-It then calls the sclclr function to set the clock line low. 
-Finally, it pops the BC register off the stack and returns.
+;It starts by pushing the BC register onto the stack. 
+;It then sets the SDA line to be an input and the SCL line to be an output. 
+;It then calls the sclset function to set the clock line high. 
+;It reads the SDA line to get the data bit and then stores it in the C register. 
+;It then calls the sclclr function to set the clock line low. 
+;Finally, it pops the BC register off the stack and returns.
 
 
 getbyte:	; Read 8 bits from i2c bus
@@ -310,10 +310,10 @@ gb1:    call    sclset          ; Clock UP
 
 
 ; The code sends a byte from the A register to the i2C bus. 
-It uses the push and pop commands to save and restore the contents of the BC register. 
-It then loads the A register into C and B, and sets B to 8. 
-It loops 8 times, shifting the bits in C and sending them to the i2C bus. 
-After the 8th iteration, it sets the SDA line high and restores the contents of BC.
+;It uses the push and pop commands to save and restore the contents of the BC register. 
+;it then loads the A register into C and B, and sets B to 8. 
+;It loops 8 times, shifting the bits in C and sending them to the i2C bus. 
+;After the 8th iteration, it sets the SDA line high and restores the contents of BC.
 
 putbyte: 	; Send byte from A to i2C bus
         push    bc
@@ -331,9 +331,9 @@ pbks1:  sla     c               ;B[7] => CY
 
 
 ; This code is for the clock signal (SCL) and data signal (SDA) of the I2C bus.
-The clock signal is generated by toggling the SCL line from high to low.
-The data signal is set by copying the state of the carry flag (CY) to the SDA line, without changing the state of the SCL line.
-The stop sequence is generated by setting the SDA line high while the SCL line is high.
+;The clock signal is generated by toggling the SCL line from high to low.
+;The data signal is set by copying the state of the carry flag (CY) to the SDA line, without changing the state of the SCL line.
+;The stop sequence is generated by setting the SDA line high while the SCL line is high.
 
 
 sclclk:         ;	"Clock" the SCL line Hi -> Lo
@@ -360,21 +360,21 @@ stop:           ; i2c STOP sequence, SDA goes HI while SCL is HI
 
 
 ;The code is written in assembly language for the 8051 microcontroller. 
-The start_i2c label indicates the start of the I2C start sequence. 
-The sequence starts with the SDA (serial data) line going LOW while the SCL (serial clock) line is HIGH. 
-This is accomplished by calling the sdaset and sclset subroutines. 
-The sdaset subroutine sets the SDA line HIGH, while the sclset subroutine sets the SCL line HIGH. 
-Next, the code calls the sdaclr and sclclr subroutines. 
-These two subroutines are used to set the SDA and SCL lines LOW, respectively. 
-Finally, the code calls the sdaset subroutine one last time to set the SDA line HIGH, and then returns. 
-The sclset, sclclr, sdaset, and sdaclr subroutines are used to manipulate the state of the SCL and SDA lines 
-without affecting the state of the other line. 
-For example, the sclset subroutine sets the SCL line HIGH without changing the state of the SDA line. 
-Each of these subroutines starts by reading the value of the PIO_C register into the A register. 
-The PIO_C register controls the state of the I/O pins on the 8051 microcontroller. 
-Next, the code sets or clears the SCL or SDA bit in the A register, 
-depending on which subroutine is being executed. 
-Finally, the code writes the new value of the A register back to the PIO_C register, which updates the state of the I/O pins.
+;The start_i2c label indicates the start of the I2C start sequence. 
+;The sequence starts with the SDA (serial data) line going LOW while the SCL (serial clock) line is HIGH. 
+;This is accomplished by calling the sdaset and sclset subroutines. 
+;The sdaset subroutine sets the SDA line HIGH, while the sclset subroutine sets the SCL line HIGH. 
+;Next, the code calls the sdaclr and sclclr subroutines. 
+;These two subroutines are used to set the SDA and SCL lines LOW, respectively. 
+;Finally, the code calls the sdaset subroutine one last time to set the SDA line HIGH, and then returns. 
+;The sclset, sclclr, sdaset, and sdaclr subroutines are used to manipulate the state of the SCL and SDA lines 
+;without affecting the state of the other line. 
+;For example, the sclset subroutine sets the SCL line HIGH without changing the state of the SDA line. 
+;Each of these subroutines starts by reading the value of the PIO_C register into the A register. 
+;The PIO_C register controls the state of the I/O pins on the 8051 microcontroller. 
+;Next, the code sets or clears the SCL or SDA bit in the A register, 
+;depending on which subroutine is being executed. 
+;Finally, the code writes the new value of the A register back to the PIO_C register, which updates the state of the I/O pins.
 
 start_i2c:          ; i2c START sequence, SDA goes LO while SCL is HI
 			call	sdaset
@@ -412,7 +412,7 @@ sdaclr: ; SDA LO without changing SCL
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-convert all the above to pseudo code
+;convert all the above to pseudo code
 
 
 I2C START SEQUENCE
